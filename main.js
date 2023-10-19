d1e57af1473640c2735b61b15f741e677a68ef91
Isabelamds <106815978+isabelamds007@users.noreply.github.com>
2023-10-19 11:09:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function tocaSom (seletorAudio) {
-    cont elemento = docment.querySelector(seletorAudio);
+    const elemento = docment.querySelector(seletorAudio);
 
     if (elemento && elemento.elemento.localName === 'audio') {
         elemento.play();
@@ -9,4 +9,27 @@ function tocaSom (seletorAudio) {
         console.log('Elemento não enontredo ou seletor inválido');
     }
     
+}
+
+const listaDeteclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeteclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = '#som_${instrumento}'; //template string
+
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydwn = function (evento) {
+
+        if (evento.code === 'Space'|| evento.code === 'Enter')
+         tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
